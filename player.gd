8b4a8fbbbb27358de60b5ed58594b5df27422ef6
KJ <kjk@skyhopper.net>
2025-04-16 19:05:28 -0400

    Initial commit

---
+++ player.gd
@@ -0,0 +1,47 @@
+extends CharacterBody3D
+
+@onready var neck: Node3D = $Neck
+@onready var camera: Camera3D = $Neck/Camera3D
+
+@export var sensitivity := 0.01
+@export var SPEED := 5.0
+@export var JUMP_VELOCITY := 2.5
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	elif event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		neck.rotate_y(-event.relative.x * sensitivity)
+		
+		# cant rotate camera vertical forever
+		const angle_lock := PI/2.1
+		if not(camera.get_rotation().x >= angle_lock or camera.get_rotation().x <= -angle_lock):
+			camera.rotate_x(-event.relative.y*sensitivity)
+		
+	elif event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
+
+func _physics_process(delta: float) -> void:
+	# Add gravity
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	
+	if not is_on_floor():
+		return
+
+	# Handle movement
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back") # gets global input directions
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized() # input direction with respect to current rotation
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED*2)
+		velocity.z = move_toward(velocity.z, 0, SPEED*2)
+
+	move_and_slide()
